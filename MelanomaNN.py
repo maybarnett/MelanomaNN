bf02d6f90dc1eb54cff9a5c6a5e600045cb2853e
maybarnett <142523706+maybarnett@users.noreply.github.com>
2023-08-24 10:23:13 -0500

    Add files via upload

---
+++ MelanomaNN.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from scipy import stats
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+from PIL import Image
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import OrdinalEncoder
+
+np.random.seed(42)
+##from keras.utils.np_utils import to_categorical 
+from sklearn.model_selection import train_test_split
+from scipy import stats
+from sklearn.preprocessing import LabelEncoder
+# import autokeras as ak
+
+import keras
+##from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPool2D, BatchNormalization
+from sklearn.model_selection import train_test_split
+from scipy import stats
+from sklearn.preprocessing import LabelEncoder
+
+import pandas as pd
+
